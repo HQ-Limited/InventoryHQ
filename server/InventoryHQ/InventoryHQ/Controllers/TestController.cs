318c81950982475fc35097492009cf6030e9d06c
unknown <frexcho998@gmail.com>
2025-05-02 13:54:48 +0300

    feat(server): create app and setup db configurations

---
+++ server/InventoryHQ/InventoryHQ/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryHQ.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            return Ok("Test");
+        }
+    }
+}
---
+++ server/InventoryHQ/InventoryHQ/Data/InventoryHQDbContext.cs
@@ -0,0 +1,13 @@
+﻿using InventoryHQ.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryHQ.Data
+{
+    public class InventoryHQDbContext : DbContext
+    {
+        public InventoryHQDbContext(DbContextOptions<InventoryHQDbContext> options) 
+            : base(options) { }
+
+        public DbSet<TestModel> Test { get; set; }
+    }
+}
