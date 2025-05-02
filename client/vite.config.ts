2c30c376c43e9e471f3f059d3f5e20d34dd4ac46
Hristiyan Simeonov <newhappyrabbit3@gmail.com>
2025-05-02 20:39:06 +0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:5042', // Backend server
+                target: 'http://localhost:5042',
                 changeOrigin: true,
             },
         },
