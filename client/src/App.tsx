8b1b116da9a1d3fa0079f221cae3c937ad4045cf
NewHappyRabbit <31893343+NewHappyRabbit@users.noreply.github.com>
2025-05-02 20:37:30 +0300

    chore(client-app): added reverse proxy for api calls

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { Button } from 'antd';
 
 function App() {
-  axios.get('/api/test').then((res) => {
-    console.log(res);
-  });
+    axios.get('/api/test').then((res) => {
+        console.log(res);
+    });
 
-  return <Button type="primary">Test</Button>;
+    return <Button type="primary">Test</Button>;
 }
 
 export default App;
