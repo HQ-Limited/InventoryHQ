8fc774c5937644bc6df05bf7a0a1a5d5a2eef9e1
NewHappyRabbit <31893343+NewHappyRabbit@users.noreply.github.com>
2025-05-21 22:59:17 +0300

    Made a mockup for partners view tables, update/create form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,32 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
-import View from './pages/products/View';
-import Create from './pages/products/Create';
+import { ConfigProvider, theme } from 'antd';
+import ProductsView from './pages/products/View';
+import ProductsCreate from './pages/products/Create';
+import PartnersView from './pages/partners/View';
+import PartnersCreateEdit from './pages/partners/CreateEdit';
 
 function App() {
     return (
-        <BrowserRouter>
-            {/* <MainLayout> */}
-            <Routes>
-                <Route path="/" element={<div>Home</div>} />
-                <Route path="/products" element={<View />} />
-                <Route path="/products/create" element={<Create />} />
-            </Routes>
-            {/* </MainLayout> */}
-        </BrowserRouter>
+        <ConfigProvider
+            theme={{
+                algorithm: theme.darkAlgorithm,
+            }}
+        >
+            <BrowserRouter>
+                {/* <MainLayout> */}
+                <Routes>
+                    <Route path="/" element={<div>Home</div>} />
+                    <Route path="/products" element={<ProductsView />} />
+                    <Route path="/products/create" element={<ProductsCreate />} />
+
+                    <Route path="/partners" element={<PartnersView />} />
+                    <Route path="/partners/new" element={<PartnersCreateEdit />} />
+                    <Route path="/partners/:id" element={<PartnersCreateEdit />} />
+                </Routes>
+                {/* </MainLayout> */}
+            </BrowserRouter>
+        </ConfigProvider>
     );
 }
 
