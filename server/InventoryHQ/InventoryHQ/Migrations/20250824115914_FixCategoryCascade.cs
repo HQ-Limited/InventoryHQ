5f170c079a294b4b45a192c61d28154998286517
NewHappyRabbit <31893343+NewHappyRabbit@users.noreply.github.com>
2025-08-24 15:01:28 +0300

    Added cascade delete

---
+++ server/InventoryHQ/InventoryHQ/Migrations/20250824115914_FixCategoryCascade.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryHQ.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCategoryCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Categories_ParentId",
+                table: "Categories");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Categories_ParentId",
+                table: "Categories",
+                column: "ParentId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Categories_ParentId",
+                table: "Categories");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Categories_ParentId",
+                table: "Categories",
+                column: "ParentId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+        }
+    }
+}
